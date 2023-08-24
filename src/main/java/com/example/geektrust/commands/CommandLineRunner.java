e13bef61cf513843d32931b2948111dfec3cb83a
Anubhav <reachanubhav24@gmail.com>
2023-08-24 14:10:39 +0530

    adding StopRide command

--- src/main/java/com/example/geektrust/commands/CommandLineRunner.java
+++ src/main/java/com/example/geektrust/commands/CommandLineRunner.java
@@ -4,6 +4,7 @@ import com.example.geektrust.commands.command_spec.AddDriverCommand;
 import com.example.geektrust.commands.command_spec.AddRiderCommand;
 import com.example.geektrust.commands.command_spec.MatchCommand;
 import com.example.geektrust.commands.command_spec.StartRideCommand;
+import com.example.geektrust.commands.command_spec.StopRideCommand;
 import com.example.geektrust.constants.CommandKeywords;
 import com.example.geektrust.registers.CommandRegistry;
 import com.example.geektrust.registers.DriverRegistry;
@@ -30,11 +31,13 @@ public class CommandLineRunner {
         AddRiderCommand addRiderCommand = new AddRiderCommand(riderRegistry);
         MatchCommand matchCommand = new MatchCommand(driverRegistry, riderRegistry);
         StartRideCommand startRideCommand = new StartRideCommand(riderRegistry, rideRegistry);
+        StopRideCommand stopRideCommand = new StopRideCommand(rideRegistry);
 
         commandRegistry.setCommand(CommandKeywords.ADD_DRIVER, addDriverCommand);
         commandRegistry.setCommand(CommandKeywords.ADD_RIDER, addRiderCommand);
         commandRegistry.setCommand(CommandKeywords.MATCH, matchCommand);
         commandRegistry.setCommand(CommandKeywords.START_RIDE, startRideCommand);
+        commandRegistry.setCommand(CommandKeywords.STOP_RIDE, stopRideCommand);
     }
 
     public void run(String file) {
--- src/main/java/com/example/geektrust/commands/command_spec/StopRideCommand.java
+++ src/main/java/com/example/geektrust/commands/command_spec/StopRideCommand.java
@@ -1,15 +1,31 @@
 package com.example.geektrust.commands.command_spec;
 
 import com.example.geektrust.commands.Command;
+import com.example.geektrust.entities.Location;
+import com.example.geektrust.entities.Ride;
+import com.example.geektrust.registers.RideRegistry;
 
 public class StopRideCommand implements Command {
+    private final RideRegistry rideRegistry;
+    private Ride ride;
+    private Location destination;
+    private Integer timeTakenInMin;
+
+    public StopRideCommand(RideRegistry rideRegistry) {
+        this.rideRegistry = rideRegistry;
+    }
+
     @Override
     public void setParams(String[] params) {
-
+        this.ride = rideRegistry.getRide(params[1]);
+        int xPos = Integer.parseInt(params[2]);
+        int yPos = Integer.parseInt(params[3]);
+        this.destination = new Location(xPos, yPos);
+        this.timeTakenInMin = Integer.parseInt(params[4]);
     }
 
     @Override
     public void execute() {
-
+//        ride.stopRide(destination, timeTakenInMin);
     }
 }
